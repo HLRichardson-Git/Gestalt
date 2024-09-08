c31cf5bc20605373b01adadc517acc75afcd1b18
Hunter Richardson <hunter.richardson2000@gmail.com>
2024-09-08 15:33:26 -0400

    Adds dedicated files for RSA KeyGen. Adds more unit tests for RSA KeyGen

---
+++ src/rsa/rsa_key_generation/rsaKeyGen.cpp
@@ -0,0 +1,123 @@
+/*
+ * Copyright 2023-2024 The Gestalt Project Authors. All Rights Reserved.
+ *
+ * Licensed under the MIT License. See the file LICENSE for the full text.
+ */
+
+/*
+ * rsaKeyGen.cpp
+ *
+ */
+
+#include "rsaKeyGen.h"
+
+bool RSAKeyPair::isPrime(const BigInt& number) {
+    if (mpz_cmp_ui(number.n, 0) == 0) {
+        return false;  // Handle the case where number is 0
+    }
+    return mpz_probab_prime_p(number.n, 5) != 0;  // Returns non-zero if the number is probably prime
+}
+
+bool RSAKeyPair::validatePrivateKey(RSAPrivateKey privateKeyCandidate) {
+    // Check that p is prime if provided
+    if (mpz_cmp_ui(privateKeyCandidate.p.n, 0) != 0) {
+        if (!isPrime(privateKeyCandidate.p)) {
+            throw std::invalid_argument("'p' is not prime.");
+        }
+    }
+
+    // Check that q is prime if provided
+    if (mpz_cmp_ui(privateKeyCandidate.q.n, 0) != 0) {
+        if (!isPrime(privateKeyCandidate.q)) {
+            throw std::invalid_argument("'q' is not prime.");
+        }
+    }
+
+    // Check if p and q are coprime if both are provided
+    if (mpz_cmp_ui(privateKeyCandidate.p.n, 0) != 0 && mpz_cmp_ui(privateKeyCandidate.q.n, 0) != 0) {
+        BigInt gcdPQ;
+        mpz_gcd(gcdPQ.n, privateKeyCandidate.p.n, privateKeyCandidate.q.n);
+        if (mpz_cmp_ui(gcdPQ.n, 1) != 0) {
+            throw std::invalid_argument("'p' and 'q' are not coprime.");
+        }
+    }
+
+    unsigned int dBitLength = mpz_sizeinbase(privateKeyCandidate.d.n, 2);
+    if (abs(int (dBitLength - static_cast<int>(specifiedStrength))) > 10) { // within specified size +-10
+        throw std::invalid_argument("Private key 'd' bit length is too far from the specified strength.");
+    }
+
+    return true;
+}
+
+bool RSAKeyPair::validatePublicKey(RSAPublicKey publicKeyCandidate) {
+    unsigned int nBitLength = mpz_sizeinbase(publicKeyCandidate.n.n, 2);
+    if (abs(int (nBitLength - static_cast<int>(specifiedStrength))) > 10) { // within specified size +-10
+        throw std::invalid_argument("Public key modulus 'n' bit length is too far from the specified strength.");
+    }
+
+    BigInt twoPow256 = "115792089237316195423570985008687907853269984665640564039457584007913129639936";
+    if (mpz_cmp_ui(publicKeyCandidate.e.n, 65536) < 0 || mpz_cmp(publicKeyCandidate.e.n, twoPow256.n) > 0) {
+        throw std::invalid_argument("Public exponent 'e' is out of the allowed range.");
+    }
+
+    return true;
+}
+
+void RSAKeyPair::computePrivateExponent(mpz_t d, const mpz_t e, const mpz_t phi_n) {
+    if (mpz_invert(d, e, phi_n) == 0) {
+        std::cerr << "Error: e has no modular inverse with respect to phi(n)" << std::endl;
+        exit(1);
+    }
+}
+
+void RSAKeyPair::generateKeyPair(RSAKeyGenOptions options) {
+    mpz_t p, q, n;
+    mpz_inits(p, q, n, NULL);
+
+    gmp_randstate_t state;
+    gmp_randinit_default(state);
+    gmp_randseed_ui(state, time(NULL));
+
+    generateLargePrime(p, static_cast<unsigned int>(options.securityStrength) / 2, options.primeMethod, state);
+    generateLargePrime(q, static_cast<unsigned int>(options.securityStrength) / 2, options.primeMethod, state);
+
+    mpz_mul(n, p, q);
+
+    // Calculate phi(n) = (p-1) * (q-1)
+    mpz_t p_minus_1, q_minus_1, phi_n;
+    mpz_inits(p_minus_1, q_minus_1, phi_n, NULL);
+    mpz_sub_ui(p_minus_1, p, 1);
+    mpz_sub_ui(q_minus_1, q, 1);
+    mpz_mul(phi_n, p_minus_1, q_minus_1);
+
+    computePrivateExponent(privateKey.d.n, publicKey.e.n, phi_n);
+    mpz_set(privateKey.p.n, p);
+    mpz_set(privateKey.q.n, q);
+    privateKey.calculateCRTComponents();
+    
+    publicKey.n = n;
+
+    mpz_clears(p, q, n, phi_n, p_minus_1, q_minus_1, NULL);
+}
+
+bool RSAKeyPair::validateKeyPair() {
+    try {
+        return validatePrivateKey(this->privateKey) && validatePublicKey(this->publicKey);
+    } catch (const std::invalid_argument& e) {
+        std::cerr << "Key validation error: " << e.what() << std::endl;
+        return false;
+    }
+}
+
+void RSAKeyPair::regenerateKeyPair(const RSAKeyGenOptions& options) {
+    generateKeyPair(options);
+}
+
+unsigned int RSAKeyPair::getModulusBitLength() const {
+    return mpz_sizeinbase(publicKey.n.n, 2);
+}
+
+unsigned int RSAKeyPair::getPrivateExponentBitLength() const {
+    return mpz_sizeinbase(privateKey.d.n, 2);
+}
---
+++ src/rsa/rsa_key_generation/rsaKeyGen.h
@@ -0,0 +1,126 @@
+/*
+ * Copyright 2023-2024 The Gestalt Project Authors. All Rights Reserved.
+ *
+ * Licensed under the MIT License. See the file LICENSE for the full text.
+ */
+
+/*
+ * rsaKeyGen.h
+ *
+ */
+
+# pragma once
+
+#include "rsa/rsaObjects.h"
+#include "rsa/prime_generation/prime_generation.h"
+
+struct RSAKeyGenOptions {
+    RSA_SECURITY_STRENGTH securityStrength = RSA_SECURITY_STRENGTH::RSA_2048;
+    RANDOM_PRIME_METHOD primeMethod = RANDOM_PRIME_METHOD::probable;
+};
+
+struct RSAPrivateKey {
+    BigInt d;      // Private exponent
+    BigInt p;      // First prime factor
+    BigInt q;      // Second prime factor
+    BigInt dP;     // d mod (p-1)
+    BigInt dQ;     // d mod (q-1)
+    BigInt qInv;   // q^(-1) mod p
+
+    RSAPrivateKey() = default;
+    RSAPrivateKey(const BigInt& d) : d(d) {}
+    RSAPrivateKey(const BigInt& d, const BigInt& p, const BigInt& q)
+        : d(d), p(p), q(q) {
+        calculateCRTComponents();
+    }
+    RSAPrivateKey(const BigInt& d, const BigInt& p, const BigInt& q, 
+              const BigInt& dP, const BigInt& dQ, const BigInt& qInv)
+    : d(d), p(p), q(q), dP(dP), dQ(dQ), qInv(qInv) {}
+
+    void calculateCRTComponents() {
+        BigInt pMinus1 = p - 1;
+        BigInt qMinus1 = q - 1;
+        dP = d % pMinus1;
+        dQ = d % qMinus1;
+        if (mpz_invert(qInv.n, q.n, p.n) == 0) {
+            // If the return value is 0, it means the inverse doesn't exist (q and p are not coprime)
+            throw std::runtime_error("q and p are not coprime, modular inverse does not exist.");
+        }
+    }
+
+    void debugCRTComponents() const {
+        std::cout << "d: " << d.toHexString() << std::endl;
+        std::cout << "p: " << p.toHexString() << std::endl;
+        std::cout << "q: " << q.toHexString() << std::endl;
+        std::cout << "dP: " << dP.toHexString() << std::endl;
+        std::cout << "dQ: " << dQ.toHexString() << std::endl;
+        std::cout << "qInv: " << qInv.toHexString() << std::endl;
+    }
+};
+
+struct RSAPublicKey {
+    BigInt n;
+    BigInt e = 65537;
+
+    RSAPublicKey() = default;
+    RSAPublicKey(const BigInt& n, const BigInt& e)
+    : n(n), e(e) {}
+};
+
+class RSAKeyPair {
+private:
+    RSAPrivateKey privateKey;
+    RSAPublicKey publicKey;
+    RSA_SECURITY_STRENGTH specifiedStrength;
+
+    bool isPrime(const BigInt& number);
+    bool validatePrivateKey(RSAPrivateKey privateKeyCandidate);
+    bool validatePublicKey(RSAPublicKey publicKeyCandidate);
+    void computePrivateExponent(mpz_t d, const mpz_t e, const mpz_t phi_n);
+    void generateKeyPair(RSAKeyGenOptions options);
+
+    friend class RSA;
+    friend class RSA_KeyPair_Test;
+public:
+
+    RSAKeyPair() { 
+        generateKeyPair({RSA_SECURITY_STRENGTH::RSA_2048, RANDOM_PRIME_METHOD::probable}); 
+    };
+    RSAKeyPair(RSAKeyGenOptions options) { 
+        generateKeyPair(options);
+    };
+    
+    RSAKeyPair(RSA_SECURITY_STRENGTH specifiedStrength, 
+               const RSAPrivateKey& privateKeyCandidate, 
+               const RSAPublicKey& publicKeyCandidate)   
+        : specifiedStrength(specifiedStrength) {
+        try {
+            validatePrivateKey(privateKeyCandidate);  // Throws if invalid
+            validatePublicKey(publicKeyCandidate);    // Throws if invalid
+            privateKey = privateKeyCandidate;
+            publicKey = publicKeyCandidate;
+        } catch (const std::invalid_argument& e) {
+            std::cerr << "Key validation error: " << e.what() << std::endl;
+            throw; // Re-throw the exception to signal the error to the caller
+        }
+    };
+
+    void setPrivateKey(RSAPrivateKey privateKeyCandidate) { 
+        if (validatePrivateKey(privateKeyCandidate)) {
+            privateKey.d = privateKeyCandidate.d;
+        }
+    };
+    void setPublicKey(RSAPublicKey publicKeyCandidate) { 
+        if (validatePublicKey(publicKeyCandidate)) {
+            publicKey.n = publicKeyCandidate.n;
+            publicKey.e = publicKeyCandidate.e;
+        }
+    };
+    RSAPrivateKey getPrivateKey() const { return privateKey; };
+    RSAPublicKey getPublicKey() const { return publicKey; };
+
+    bool validateKeyPair();
+    void regenerateKeyPair(const RSAKeyGenOptions& options);
+    unsigned int getModulusBitLength() const;
+    unsigned int getPrivateExponentBitLength() const;
+};
